6a25b54d9456169c7fa39a65b79ff1d0ce5899af
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-06-18 08:37:03 +0000

    sst types

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -5,18 +5,6 @@
 
 declare module "sst" {
   export interface Resource {
-    "Api": {
-      "type": "sst.aws.ApiGatewayV2"
-      "url": string
-    }
-    "Notes": {
-      "name": string
-      "type": "sst.aws.Dynamo"
-    }
-    "Uploads": {
-      "name": string
-      "type": "sst.aws.Bucket"
-    }
   }
 }
 /// <reference path="sst-env.d.ts" />
