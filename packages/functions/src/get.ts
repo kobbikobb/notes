25fc999f60ff5d5d2cffb5bf18fc261b7e3fe15b
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-06-28 07:49:53 +0000

    Add tests

--- packages/functions/src/get.ts
+++ packages/functions/src/get.ts
@@ -3,7 +3,6 @@ import { getNoteItem } from '@notes/core/repositories';
 
 export const main = Util.handler(async (event) => {
     const userId = Util.getUserId(event);
-
     const noteId = event?.pathParameters?.id;
     if (!noteId) {
         throw new Error('Note ID is required.');
