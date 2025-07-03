430126d82c39b1e64a9ca9aa64d12f9eed9023a8
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-07-03 23:38:32 +0000

    Auth

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,22 +1,38 @@
+import { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
 import Routes from './Routes.tsx';
+import { AppContext, type AppContextType } from './lib/contextLib';
 
 function App() {
+    const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+    function handleLogout() {
+        userHasAuthenticated(false);
+    }
+
     return (
         <div className="App container py-3">
             <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
                 <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
-                    <Nav>
-                        <Nav.Link href="/signup">Signup</Nav.Link>
-                        <Nav.Link href="/login">Login</Nav.Link>
-                    </Nav>
+                    {isAuthenticated ? (
+                        <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                    ) : (
+                        <>
+                            <Nav.Link href="/signup">Signup</Nav.Link>
+                            <Nav.Link href="/login">Login</Nav.Link>
+                        </>
+                    )}
                 </Navbar.Collapse>
             </Navbar>
-            <Routes />
+            <AppContext.Provider
+                value={{ isAuthenticated, userHasAuthenticated } as AppContextType}
+            >
+                <Routes />
+            </AppContext.Provider>
         </div>
     );
 }
