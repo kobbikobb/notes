817fa432f0d3c32a1584e2daffa036bd44787cea
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-07-05 23:50:38 +0000

    Signup

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './containers/Home.tsx';
 import NotFound from './containers/NotFound.tsx';
 import Login from './containers/Login.tsx';
+import Signup from './containers/Signup.tsx';
 
 export default function Links() {
     return (
@@ -10,6 +11,7 @@ export default function Links() {
             {/* Finally, catch all unmatched routes */}
             <Route path="*" element={<NotFound />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />{' '}
         </Routes>
     );
 }
